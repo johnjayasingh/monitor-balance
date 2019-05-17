f4beda2701c6085c181936c6d1a276633636d0bb
John S <johns@accubits.com>
2019-05-17 11:56:01 +0530

    report

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
-// const app = require('express')()
 const json2csv = require('json2csv')
+const fs = require('fs')
+const path = require('path')
 require('dotenv').config()
 
 const mnemonic = require('./mnemonic')
-const constants = require('./constants')
 
 mongoose.connect(`mongodb://${process.env.MONGODB_URI || 'localhost:27017'}/globalpay`, { useNewUrlParser: true });
-
-// app.listen(8888, console.info)
 
 const UserSchema = new mongoose.Schema(
     {
@@ -19,13 +17,18 @@ const UserSchema = new mongoose.Schema(
 
 const User = mongoose.model("GlobalPayUsers", UserSchema);
 
-User.find().lean().limit(Number(process.env.LIMIT) || 1).select('-_id userId email phone').then(async dbResult => {
+User.find().lean().skip(Number(process.env.SKIP)).limit(Number(process.env.LIMIT) || 1).select('-_id userId email phone').then(async dbResult => {
     const promise = []
     dbResult.forEach(data => {
         promise.push(mnemonic.getAccountCredentials(data.userId).then(result => ({ Ethereum: result.ETH.publicKey, 'Ethereum Balance': result.ETH.balance, Bitcoin: result.BTC.publicKey, 'Bitcoin Balance': result.BTC.balance, ...data })))
     })
     const resolvedAddress = await Promise.all(promise).catch(console.error)
     const result = json2csv.parse(resolvedAddress)
+    try {
+        fs.writeFileSync(path.resolve(__dirname, `output_${new Date().toUTCString()}.csv`), result)
+    } catch (error) {
+        process.exit(0)
+    }
     console.log(result)
     process.exit()
 })
